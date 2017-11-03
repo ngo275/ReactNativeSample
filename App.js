bde33a066dc02340ac800ad518bc0e9b70b744a7
ngo275 <ngo38114@gmail.com>
2017-11-03 21:00:33 +0900

    display listview of articles

--- App.js
+++ App.js
@@ -10,26 +10,93 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Image,
+  Button,
+  ListView,
+  TouchableHighlight
 } from 'react-native';
 import { StackNavigator, TabNavigator } from 'react-navigation';
 
+const END_POINT = 'https://qiita.com/api/v2/tags/reactjs/items';
+
 class ItemsScreen extends React.Component {
+
   static navigationOptions = {
     title: 'Items'
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false
+    };
+    this.renderItem = this.renderItem.bind(this);
+  }
   render() {
     const { navigate } = this.props.navigation;
+    const site = this.state.loaded;
+    if (!site) {
+      return this.renderLoadingView();
+    }
     return (
       <View>
-        <Text>hogehoge</Text>
-        <Button
-          onPress={() => navigate('Detail', { item: 'Itemkk' })}
-          title='Show'
+        <ListView
+          dataSource={this.state.items}
+          renderRow={this.renderItem}
+          renderSeparator={this.renderSeparator}
+          onEndReached={() => { console.log('End!!!'); }}
         />
       </View>
     );
   }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    fetch(END_POINT)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          items: this.state.items.cloneWithRows(responseData),
+          loaded: true
+        })
+      })
+      .done()
+  }
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+  renderSeparator(sectionId, rowId) {
+    return <View key={rowId} style={styles.separator}/>;
+  }
+  renderItem(item) {
+    const { navigate } = this.props.navigation;
+    console.log(this.props.navigation);
+    return (
+      <TouchableHighlight onPress={() => { navigate('item', { user: 'Lucy' }) }} >
+        <View style={styles.container}>
+          <Image
+            style={styles.thumbnail}
+            source={{uri: item.user.profile_image_url}}
+          />
+          <View style={styles.rightContainer}>
+            {/*<Button*/}
+              {/*onPress={() => navigate('item', { user: 'Lucy' })}*/}
+              {/*title='Show'*/}
+            {/*/>*/}
+            <Text style={styles.title}>{item.title}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
 }
 
 class DetailScreen extends React.Component {
@@ -67,9 +134,6 @@ const SimpleApp = StackNavigator({
   Detail: { screen: DetailScreen }
 });
 
-
-const END_POINT = 'https://qiita.com/api/v2/tags/reactjs/items';
-
 export default class App extends Component<{}> {
   render() {
     return <SimpleApp/>;
@@ -79,8 +143,27 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E'
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 15,
+    margin: 8,
+    textAlign: 'left'
+  },
+  thumbnail: {
+    width: 80,
+    height: 80,
+    margin: 0
+  }
 });
--- index.android.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { AppRegistry } from 'react-native'
-import ReactApp from './index.js'
-// Had you placed index.js in another folder like `./app`, you could instead do your import with this shorthand:
-// import ReactApp from './app'
-
-AppRegistry.registerComponent('ReactApp', () => ReactApp)
